1671646e811d26313a5f376eb453de236b122b45
Amon Daniel <amon.daniel.91@gmail.com>
2023-07-20 23:17:47 +0100

    Sign in and out

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,8 +1,9 @@
 import { cn } from "@/lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
 import { ButtonHTMLAttributes, FC, forwardRef } from "react";
+import { Loader2 } from "lucide-react";
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 dark:focus:ring-slate-400 disabled:pointer-events-none dark:focus:ring-offset-slate-900",
   {
     variants: {
@@ -31,18 +32,25 @@ const buttonVariants = cva(
 interface ButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  isLoading?: boolean
+  isLoading?: boolean;
 }
 
+// eslint-disable-next-line react/display-name
 const Button: FC<ButtonProps> = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children, variant, isLoading, size,...props }, ref) => {
+  ({ className, children, variant, isLoading, size, ...props }, ref) => {
     return (
       <button
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
-      ></button>
+        disabled={isLoading}
+        {...props}>
+        {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+        {children}
+      </button>
     );
   }
 );
+
+Button.displayName = 'Button'
 
 export default Button;
